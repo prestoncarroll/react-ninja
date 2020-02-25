03176d3bcb2690eedbb3f91c9dfb7f354b88ee68
Preston Carroll <plutusinc1@yahoo.com>
2020-02-24 18:17:57 -0600

    recipes added to console .. get recipes function is working

--- react-ninja/src/App.js
+++ react-ninja/src/App.js
@@ -1,21 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import './App.css';
 
-
-const APP_ID = "7dfa7298";
-const APP_KEY = "b7773e5293acb1995ec038439ba83acf";
-const exampleRequest = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
-
-
 const App = () => {
-  return (
-    <div className="App"> 
-    <form className="search-form">
-      <input className="search-bar" type="text"/>
-      <button className="search-button" type="submit">Search</button>
+  const APP_ID = "7dfa7298";
+  const APP_KEY = "b7773e5293acb1995ec038439ba83acf";
 
-    </form>
+  useEffect ( () => {
+    getRecipes();
+
+  }, []);
+
+
+
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`)
+    const data = await response.json();
+    console.log(data);
+  }
+
+
+  return (
+    <div className="App">
+      <form className="search-form">
+        <input className="search-bar" type="text" />
+        <button className="search-button" type="submit">Search</button>
+
+      </form>
     </div>
 
   );
